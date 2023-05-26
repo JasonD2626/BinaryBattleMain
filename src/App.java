d1392a5a5acabad5cafa07a3cbfff693ecf09af5
JasonD2626 <jason.dominic26@gmail.com>
2023-05-25 22:35:30 -0400

    added text

--- src/App.java
+++ src/App.java
@@ -1,14 +1,24 @@
+import java.awt.Font;
+
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class App {
     public static void main(String[] args) throws Exception {
 
+        JPanel panel = new JPanel();        
         JFrame frame = new JFrame();
-        frame.setSize(100, 100);
+        frame.setSize(1500, 900);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
+        frame.add(panel);
 
-        JPanel panel = new JPanel();        
+        panel.setLayout(null);
+
+        JLabel label = new JLabel("BinaryBattle");
+        label.setBounds(10, 20, 8000, 600);
+        label.setFont(new Font("Calibri", Font.BOLD, 20));
+        panel.add(label);
     }
 }
