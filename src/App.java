561695d4534fbd4eb3099779e1041074c72c9497
Jason Dominic <107736333+JasonD2626@users.noreply.github.com>
2023-05-25 21:50:25 -0400

    added frame & panel

--- src/App.java
+++ src/App.java
@@ -1,5 +1,14 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        JFrame frame = new JFrame();
+        frame.setSize(100, 100);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+        JPanel panel = new JPanel();        
     }
 }
