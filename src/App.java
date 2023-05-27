d1a0b2cbfa7ca549cf155c26065c7e733c14263c
Nitro26 <jason.dominic26@gmail.com>
2023-05-27 14:30:56 -0400

    added combobox & fixed text

--- src/App.java
+++ src/App.java
@@ -1,15 +1,18 @@
 import java.awt.Font;
 
+import javax.swing.ComboBoxModel;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 
 public class App {
     public static void main(String[] args) throws Exception {
 
         JPanel panel = new JPanel();        
         JFrame frame = new JFrame();
-        frame.setSize(1500, 900);
+        frame.setSize(1300, 600);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
         frame.add(panel);
@@ -17,8 +20,14 @@ public class App {
         panel.setLayout(null);
 
         JLabel label = new JLabel("BinaryBattle");
-        label.setBounds(10, 20, 8000, 600);
-        label.setFont(new Font("Calibri", Font.BOLD, 20));
+        label.setBounds(480, -200, 8000, 600);
+        label.setFont(new Font("Calibri", Font.BOLD, 70));
         panel.add(label);
+
+        String[] myArray = {"2", "3", "4", "5", "6", "7"};
+        
+        final JComboBox<String> myBox = new JComboBox<String>(myArray);
+        myBox.setBounds(480, -0, 50, 50);;
+        panel.add(myBox);
     }
 }
