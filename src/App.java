6400b5d106b4f24613c7ebea439ad0c3feddd59a
JasonD2626 <jason.dominic26@gmail.com>
2023-05-28 17:56:27 -0400

    added category + question headers

--- src/App.java
+++ src/App.java
@@ -19,9 +19,14 @@ public class App implements ActionListener{
     public static JLabel title = new JLabel("BinaryBattle");
     public static JLabel teams = new JLabel("How many teams?");
     public static ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6));
-    public static JComboBox<ArrayList> myBox;
+    public static ArrayList<String> questionTypes = new ArrayList<>(Arrays.asList("good morning", "b", "c", "d", "e"));
+    public static ArrayList<String> questions = new ArrayList<>(Arrays.asList("f", "g", "h", "i", "j"));
+    public static JComboBox<ArrayList> myBox1;
+    public static JComboBox<ArrayList> myBox2;
     public static JButton playButton = new JButton("Start Game");
-    public static JLabel questionType = new JLabel("Select question type: ");
+    public static JButton categoryOKButton = new JButton("OK!");
+    public static JLabel questionType = new JLabel("Select question category: ");
+    public static JLabel question = new JLabel("Select question: ");
     
 
     public static void main(String[] args) throws Exception {
@@ -49,9 +54,19 @@ public class App implements ActionListener{
         teams.setFont(new Font("Calibri", Font.BOLD, 20));
         panel.add(teams);
 
-        myBox = new JComboBox(numbers.toArray());
-        myBox.setBounds(620, 220, 50, 50);;
-        panel.add(myBox);
+        questionType.setBounds(500, -120, 8000, 600);
+        questionType.setFont(new Font("Calibri", Font.BOLD, 30));
+
+        question.setBounds(550, 50, 8000, 600);
+        question.setFont(new Font("Calibri", Font.BOLD, 30));
+
+        myBox2 = new JComboBox(questions.toArray());
+        myBox2.setBounds(570, 380, 150, 50);
+
+
+        myBox1 = new JComboBox(numbers.toArray());
+        myBox1.setBounds(570, 220, 150, 50);;
+        panel.add(myBox1);
 
         
         playButton.setBounds(550, 350, 200, 100);
@@ -77,7 +92,28 @@ public class App implements ActionListener{
         panel.removeAll();
         if (whichScreen.equals("gs"))
         {
-           frame.repaint();
+            myBox1.removeAllItems();
+            myBox1.setModel(new JComboBox(questionTypes.toArray()).getModel());
+            panel.add(myBox2);
+            panel.add(myBox1);
+            panel.add(questionType);
+            panel.add(question);
+            panel.revalidate();
+            panel.repaint();
+            frame.repaint();
+
+   
+        }
+        
+    }
+
+    public void setQuestions(String whichQuestion)
+    {
+        if (whichQuestion.equals("a"))
+        {
+            myBox1.removeAllItems();
         }
     }
+
+
 }
