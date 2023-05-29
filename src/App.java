c5cc1e685e0e770d31b1a777de3e1690cacef212
JasonD2626 <jason.dominic26@gmail.com>
2023-05-29 17:43:01 -0400

    added category & point selections

--- src/App.java
+++ src/App.java
@@ -19,14 +19,18 @@ public class App implements ActionListener{
     public static JLabel title = new JLabel("BinaryBattle");
     public static JLabel teams = new JLabel("How many teams?");
     public static ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6));
-    public static ArrayList<String> questionTypes = new ArrayList<>(Arrays.asList("good morning", "b", "c", "d", "e"));
-    public static ArrayList<String> questions = new ArrayList<>(Arrays.asList("f", "g", "h", "i", "j"));
+    public static ArrayList<String> questionTypes = new ArrayList<>(Arrays.asList("cat1", "cat2", "cat3", "cat4", "cat5"));
+    public static ArrayList<String> questions1 = new ArrayList<>(Arrays.asList("100", "200", "300", "400", "500"));
+    public static ArrayList<String> questions2 = new ArrayList<>(Arrays.asList("100", "200", "300", "400", "500"));
+    public static ArrayList<String> questions3 = new ArrayList<>(Arrays.asList("100", "200", "300", "400", "500"));
+    public static ArrayList<String> questions4 = new ArrayList<>(Arrays.asList("100", "200", "300", "400", "500"));
+    public static ArrayList<String> questions5 =new ArrayList<>(Arrays.asList("100", "200", "300", "400", "500"));
     public static JComboBox<ArrayList> myBox1;
     public static JComboBox<ArrayList> myBox2;
     public static JButton playButton = new JButton("Start Game");
-    public static JButton categoryOKButton = new JButton("OK!");
+    public static JButton categoryOKButton = new JButton("Select");
     public static JLabel questionType = new JLabel("Select question category: ");
-    public static JLabel question = new JLabel("Select question: ");
+    public static JLabel question = new JLabel("Select point value: ");
     
 
     public static void main(String[] args) throws Exception {
@@ -57,16 +61,20 @@ public class App implements ActionListener{
         questionType.setBounds(500, -120, 8000, 600);
         questionType.setFont(new Font("Calibri", Font.BOLD, 30));
 
-        question.setBounds(550, 50, 8000, 600);
+        question.setBounds(530, 50, 8000, 600);
         question.setFont(new Font("Calibri", Font.BOLD, 30));
 
-        myBox2 = new JComboBox(questions.toArray());
+        myBox2 = new JComboBox();
         myBox2.setBounds(570, 380, 150, 50);
 
 
         myBox1 = new JComboBox(numbers.toArray());
         myBox1.setBounds(570, 220, 150, 50);;
         panel.add(myBox1);
+
+        categoryOKButton.setBounds(590, 280, 100, 20);
+        categoryOKButton.setFont(new Font("Calibri", Font.BOLD, 10));
+        categoryOKButton.addActionListener(new App());
 
         
         playButton.setBounds(550, 350, 200, 100);
@@ -84,7 +92,54 @@ public class App implements ActionListener{
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        switchPanels("gs");
+        if (e.getSource() == playButton)
+        {
+            switchPanels("gs");
+        }
+        else if (e.getSource() == categoryOKButton)
+        {
+            if(((String)myBox1.getSelectedItem()).equals("cat1"))
+            {
+              myBox2.removeAllItems();
+              myBox2.setModel(new JComboBox(questions1.toArray()).getModel());
+              panel.revalidate();
+              panel.repaint();
+              frame.repaint();
+            }
+            else if(((String)myBox1.getSelectedItem()).equals("cat2"))
+            {
+              myBox2.removeAllItems();
+              myBox2.setModel(new JComboBox(questions2.toArray()).getModel());
+              panel.revalidate();
+              panel.repaint();
+              frame.repaint();
+            }
+            else if(((String)myBox1.getSelectedItem()).equals("cat3"))
+            {
+              myBox2.removeAllItems();
+              myBox2.setModel(new JComboBox(questions3.toArray()).getModel());
+              panel.revalidate();
+              panel.repaint();
+              frame.repaint();
+            }
+            else if(((String)myBox1.getSelectedItem()).equals("cat4"))
+            {
+              myBox2.removeAllItems();
+              myBox2.setModel(new JComboBox(questions4.toArray()).getModel());
+              panel.revalidate();
+              panel.repaint();
+              frame.repaint();
+            }
+            else if(((String)myBox1.getSelectedItem()).equals("cat5"))
+            {
+              myBox2.removeAllItems();
+              myBox2.setModel(new JComboBox(questions5.toArray()).getModel());
+              panel.revalidate();
+              panel.repaint();
+              frame.repaint();
+            }
+            
+        }
     }
 
     public void switchPanels(String whichScreen)
@@ -96,13 +151,14 @@ public class App implements ActionListener{
             myBox1.setModel(new JComboBox(questionTypes.toArray()).getModel());
             panel.add(myBox2);
             panel.add(myBox1);
+            panel.add(categoryOKButton);
             panel.add(questionType);
             panel.add(question);
             panel.revalidate();
             panel.repaint();
             frame.repaint();
 
-   
+    
         }
         
     }
